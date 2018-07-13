54dfa922e63916165ed88c8f30c9894bf540924d
wuxinxin <wuxinxin@DESKTOP-ADVDMJ0>
2018-07-13 10:53:41 +0800

    修改社员同步数据方式

--- src/main/java/com/guoanshequ/dc/das/task/MemberCountTask.java
+++ src/main/java/com/guoanshequ/dc/das/task/MemberCountTask.java
@@ -38,47 +38,49 @@ public class MemberCountTask {
 	 * @Title: UserMemberScheduleTask @Description: 社员信息表，每小时一次，采用多线程 @param
 	 * 设定文件 @return void 返回类型 @throws
 	 */
-	//@Scheduled(cron = "0 */1 * * * ?")
 	@Scheduled(cron ="0 58 2 * * ?")
 	public void memberCountTask() {
 		new Thread() {
 			public void run() {
 				try {
-					logger.info("************每小时更新成交额成交量任务开始***********************");
-
-					Map<String, String> paraMap = new HashMap<String, String>();
-
-					// 从gemini中查询成交量
-					List<Map<String, Object>> memberCountList = memberCountService.queryMemberCount(paraMap);
-					if (!memberCountList.isEmpty()) {
-						// 设置任务为运行中状态
-						Map<String, String> runMap = new HashMap<String, String>();
-						runMap.put("member_type", "1");
-						for (Map<String, Object> memberCount : memberCountList) {
-							runMap.put("member_count", memberCount.get("cou").toString());
-							// 更新数据
-							dfMemberCountService.updateMemberCount(runMap);
-						}
-					}
-					// 从gemini中查询成交额
-					List<Map<String, Object>> memberSumList = memberCountService.queryMemberSum(paraMap);
-					if (!memberSumList.isEmpty()) {
-						// 设置任务为运行中状态
-						Map<String, String> memberSumMap = new HashMap<String, String>();
-						memberSumMap.put("member_type", "2");
-						for (Map<String, Object> memberSum : memberSumList) {
-							memberSumMap.put("member_count", memberSum.get("cou").toString());
-							// 更新数据
-							dfMemberCountService.updateMemberCount(memberSumMap);
-						}
-					}
-					Map<String, String> selMap = new HashMap<String, String>();
+					logger.info("************成交额成交量任务开始***********************");
 					List<String> fileIdList= new ArrayList<String>();
+					fileIdList.add("1");
+					fileIdList.add("2");
 					fileIdList.add("6");
 					fileIdList.add("7");
 					fileIdList.add("8");
 					fileIdList.add("9");
 					dfMemberCountService.deletMembers(fileIdList);
+					Map<String, String> paraMap = new HashMap<String, String>();
+
+					// 从gemini中查询成交量
+					List<Map<String, Object>> memberCountList = memberCountService.queryMemberCount(paraMap);
+					if (!memberCountList.isEmpty()) {
+						
+						for (Map<String, Object> memberCount : memberCountList) {
+							Map<String, String> runMap = new HashMap<String, String>();
+							runMap.put("member_type", "1");
+							runMap.put("member_count", memberCount.get("cou").toString());
+							runMap.put("city_code", memberCount.get("city_code").toString());
+							// 更新数据
+							dfMemberCountService.addDfMemberCount(runMap);
+						}
+					}
+					// 从gemini中查询成交额
+					List<Map<String, Object>> memberSumList = memberCountService.queryMemberSum(paraMap);
+					if (!memberSumList.isEmpty()) {
+						for (Map<String, Object> memberSum : memberSumList) {
+							Map<String, String> memberSumMap = new HashMap<String, String>();
+							memberSumMap.put("member_type", "2");
+							memberSumMap.put("member_count", memberSum.get("cou").toString());
+							memberSumMap.put("city_code", memberSum.get("city_code").toString());
+							// 更新数据
+							dfMemberCountService.addDfMemberCount(memberSumMap);
+						}
+					}
+					Map<String, String> selMap = new HashMap<String, String>();
+
 					//查询e店订单量、成交额
 					List<Map<String, Object>> eShopCountList = memberCountService.queryEshopCount(paraMap);
 					if (!eShopCountList.isEmpty()) {
@@ -96,13 +98,13 @@ public class MemberCountTask {
 					//查询e店社员、非社员成交额
 					List<Map<String, Object>> allEshopSumList = memberCountService.queryAllEshopSum(paraMap);
 					if (!allEshopSumList.isEmpty()) {
-						Map<String, String> allEshopSumMap = new HashMap<String, String>();
-						allEshopSumMap.put("member_type", "7");
 						for (Map<String, Object> allEshopSum : allEshopSumList) {
+							Map<String, String> allEshopSumMap = new HashMap<String, String>();
+							allEshopSumMap.put("member_type", "7");
 							allEshopSumMap.put("member_name", "allEshopSum");
 							allEshopSumMap.put("member_count", allEshopSum.get("cou").toString());
 							allEshopSumMap.put("sell_duration", "0");
-							allEshopSumMap.put("remark", "e店社员、非社员总成交额");
+							allEshopSumMap.put("city_code", allEshopSum.get("city_code").toString());
 							// 入库
 							dfMemberCountService.addDfMemberCount(allEshopSumMap);
 						}
@@ -110,12 +112,12 @@ public class MemberCountTask {
 					//查询e店社员成交额
 					List<Map<String, Object>> yesEshopSumList = memberCountService.queryYesEshopSum(paraMap);
 					if (!yesEshopSumList.isEmpty()) {
-						Map<String, String> yesEshopSumMap = new HashMap<String, String>();
-						yesEshopSumMap.put("member_type", "8");
 						for (Map<String, Object> yesEshopSum : yesEshopSumList) {
+							Map<String, String> yesEshopSumMap = new HashMap<String, String>();
+							yesEshopSumMap.put("member_type", "8");
 							yesEshopSumMap.put("member_name", "yesEshopSum");
 							yesEshopSumMap.put("member_count", yesEshopSum.get("cou").toString());
-							yesEshopSumMap.put("remark", "e店社员成交额");
+							yesEshopSumMap.put("city_code", yesEshopSum.get("city_code").toString());
 							// 入库
 							dfMemberCountService.addDfMemberCount(yesEshopSumMap);
 						}
@@ -123,19 +125,19 @@ public class MemberCountTask {
 					//查询e店非社员成交额
 					List<Map<String, Object>> noEshopSumList = memberCountService.queryNoEshopSum(paraMap);
 					if (!noEshopSumList.isEmpty()) {
-						Map<String, String> noEshopSumMap = new HashMap<String, String>();
-						noEshopSumMap.put("member_type", "9");
 						for (Map<String, Object> noEshopSum : noEshopSumList) {
+							Map<String, String> noEshopSumMap = new HashMap<String, String>();
+							noEshopSumMap.put("member_type", "9");
 							noEshopSumMap.put("member_name", "noEshopSum");
 							noEshopSumMap.put("member_count", noEshopSum.get("cou").toString());
-							noEshopSumMap.put("remark", "e店非社员成交额");
+							noEshopSumMap.put("city_code", noEshopSum.get("city_code").toString());
 							// 入库
 							dfMemberCountService.addDfMemberCount(noEshopSumMap);
 						}
 					}
 					logger.info("************社员信息任务结束***********************");
 				} catch (Exception e) {
-					logger.info("社员信息任务出现问题，任务执行失败，请查看！");
+					logger.info("社员信息任务执行失败，请查看！");
 					logger.info(e.toString());
 					e.printStackTrace();
 				}
@@ -213,7 +215,7 @@ public class MemberCountTask {
 							int sellDuration = getMargin(nowDate, createTime);
 							coolMap.put("sell_duration", sellDuration + "");
 							if(membercool.get("citycode")!=null) {
-								coolMap.put("remark", membercool.get("citycode").toString());//城市id
+								coolMap.put("city_code", membercool.get("citycode").toString());//城市id
 							}
 							// 入库
 							dfMemberCountService.addDfMemberCount(coolMap);
@@ -223,12 +225,12 @@ public class MemberCountTask {
 					//查询动销商品量
 					List<Map<String, Object>> movingPinList = memberCountService.queryMovingPin(paraMap);
 					if (!movingPinList.isEmpty()) {
-						Map<String, String> movingPinMap = new HashMap<String, String>();
-						movingPinMap.put("member_type", "5");
 						for (Map<String, Object> movingPin : movingPinList) {
+							Map<String, String> movingPinMap = new HashMap<String, String>();
+							movingPinMap.put("member_type", "5");
 							movingPinMap.put("member_name", "movingPin");
 							movingPinMap.put("member_count", movingPin.get("cou").toString());
-							movingPinMap.put("remark", "动销商品数量");
+							movingPinMap.put("city_code", movingPin.get("city_code").toString());
 							// 入库
 							dfMemberCountService.addDfMemberCount(movingPinMap);
 						}
@@ -239,9 +241,9 @@ public class MemberCountTask {
 
 					
 					
-					logger.info("************社员信息任务结束***********************");
+					logger.info("************每天更最受欢迎、无人问津商品任务结束***********************");
 				} catch (Exception e) {
-					logger.info("社员商品统计出现问题，任务执行失败，请查看！");
+					logger.info("每天更最受欢迎、无人问津商品任务执行失败，请查看！");
 					logger.info(e.toString());
 					e.printStackTrace();
 				}
@@ -286,15 +288,15 @@ public class MemberCountTask {
 								protypeSumMap.put("ordertype", protype);
 								protypeSumMap.put("ordercount", ordercount.get("cou").toString());
 								protypeSumMap.put("data_date", ordercount.get("data_date").toString());
-								protypeSumMap.put("remark", protype);
+								protypeSumMap.put("city_code", ordercount.get("city_code").toString());
 								// 入库
 								dfMemberCountService.addProductCount(protypeSumMap);
 							}
 						}
 					}
-					logger.info("************商品分类订单数据结束***********************");
+					logger.info("************更新商品分类订单数据任务结束***********************");
 				} catch (Exception e) {
-					logger.info("商品分类订单数据出现问题，任务执行失败，请查看！");
+					logger.info("更新商品分类订单数据任务执行失败，请查看！");
 					logger.info(e.toString());
 					e.printStackTrace();
 				}
@@ -334,9 +336,9 @@ public class MemberCountTask {
 								dfMemberCountService.addOrderHourCount(protypeSumMap);
 							}
 						}
-					logger.info("************按小时统计订单量结束***********************");
+					logger.info("************更新按小时统计订单量任务结束***********************");
 				} catch (Exception e) {
-					logger.info("按小时统计订单量统计出现问题，任务执行失败，请查看！");
+					logger.info("更新按小时统计订单量任务执行失败，请查看！");
 					logger.info(e.toString());
 					e.printStackTrace();
 				}
@@ -351,7 +353,8 @@ public class MemberCountTask {
 	 * @author wuxinxin
 	 */
 	
-	@Scheduled(cron ="0 25 3 15 * ?")
+	//@Scheduled(cron ="0 25 3 15 * ?")
+	@Scheduled(cron = "30 * * * * ?")
 	public void eshopInfoTask() {
 		new Thread() {
 			public void run() {
@@ -373,7 +376,7 @@ public class MemberCountTask {
 						}
 					logger.info("************更新安心合作社结束***********************");
 				} catch (Exception e) {
-					logger.info("商品分类订单数据出现问题，任务执行失败，请查看！");
+					logger.info("更新安心合作社执行失败，请查看！");
 					logger.info(e.toString());
 					e.printStackTrace();
 				}
