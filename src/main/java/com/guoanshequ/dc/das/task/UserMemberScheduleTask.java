7ae9fdfbc81eaab3e2d852037ed2a3446673da46
zhaoxuguang <zhaoxuguang@guoanshequ.com>
2019-01-03 14:15:08 +0800

    修改定时时间为10点

--- src/main/java/com/guoanshequ/dc/das/task/UserMemberScheduleTask.java
+++ src/main/java/com/guoanshequ/dc/das/task/UserMemberScheduleTask.java
@@ -456,7 +456,7 @@ public class UserMemberScheduleTask {
 	 * @throws
 	 * cron:  0 0 8 * * ? *
 	 */
-	@Scheduled(cron = "0 0 15 * * ? *")
+	@Scheduled(cron = "0 0 10 * * ? *")
 	public void sendCareerMessageTask() {
 		try {
 				logger.info(">>>>>>>>>>>>>>>>>开始发频道负责人---sendshortmessagestart......... ");
